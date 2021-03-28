7716fc56a9835ce54019068bafe2393392482359
Brigid C <b@c.com>
2021-03-28 13:58:19 -0500

    one-to-many

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,13 +1,20 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class EventCategory extends AbstractEntity{
 
-	@NotBlank(message = "Must be at least 3 characters")
+	@Size(min=3,message = "Name must be at least 3 characters long")
 	private String name;
+
+	@OneToMany(mappedBy = "eventCategory")
+	private final List<Event> events = new ArrayList<>();
 
 	public EventCategory(String name) {
 		this.name = name;
@@ -21,6 +28,10 @@ public class EventCategory extends AbstractEntity{
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public List<Event> getEvents() {
+		return events;
 	}
 
 	@Override
