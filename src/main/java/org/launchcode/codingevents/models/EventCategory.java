4c9e3cf24017c16bd368b972f3b0fd624db08a92
Brigid C <b@c.com>
2021-03-28 01:29:55 -0500

    created the EventCategory Controller, Class and Repository

---
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -0,0 +1,28 @@
+package org.launchcode.codingevents.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class EventCategory {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	private String name;
+
+	public EventCategory(String name) {
+		this.name = name;
+	}
+	public EventCategory(){}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
