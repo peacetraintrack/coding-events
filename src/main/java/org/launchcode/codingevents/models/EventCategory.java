3bf88cdb2b30fde37bf88e20842a39237b22ed12
Brigid C <b@c.com>
2021-03-28 13:16:07 -0500

    Many-to-One

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,16 +1,10 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
 @Entity
-public class EventCategory {
-
-	@Id
-	@GeneratedValue
-	private int id;
+public class EventCategory extends AbstractEntity{
 
 	@NotBlank(message = "Must be at least 3 characters")
 	private String name;
@@ -27,5 +21,10 @@ public class EventCategory {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "name";
 	}
 }
